019d52bb53eeaa9eeaa816a8f94239447a59ff60
Jenny Wang <jenny.m.wang.work@gmail.com>
2025-10-27 19:45:43 -0700

    Update create-vpn-gateway.ps1

--- Azure-RealWorld-Labs/Lab01-Hybrid-Connectivity/scripts/create-vpn-gateway.ps1
+++ Azure-RealWorld-Labs/Lab01-Hybrid-Connectivity/scripts/create-vpn-gateway.ps1
@@ -1,7 +1,15 @@
-# Step 5: Create VPN Gateway and Public IP
-# Author: Jenny Wang (@JennyCloud)
-# Purpose: Provision a VPN Gateway with a Static Public IP for Hybrid Connectivity Lab
+# Step 4: Create Azure Virtual Network and Subnets
+New-AzVirtualNetwork `
+  -Name "Azure-VNet" `
+  -ResourceGroupName "HybridLab-RG" `
+  -Location "CanadaCentral" `
+  -AddressPrefix "10.10.0.0/16" `
+  -Subnet @(
+     @{Name="VMSubnet"; AddressPrefix="10.10.1.0/24"},
+     @{Name="GatewaySubnet"; AddressPrefix="10.10.255.0/27"}
+   )
 
+# Step 5: Create VPN Gateway and Public IP
 # Create a Standard, Static Public IP (required for VPN Gateway)
 $pip = New-AzPublicIpAddress `
     -Name "AzureVPN-PIP" `
